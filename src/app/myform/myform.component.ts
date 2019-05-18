703d16499cd827ec1558d5b2444b642468f56880
casidey <myroom23@gmail.com>
2019-05-18 13:47:31 +0800

    udpated form component

--- src/app/myform/myform.component.ts
+++ src/app/myform/myform.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
+import { pipe } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-myform',
@@ -8,15 +10,38 @@ import { FormControl } from '@angular/forms';
 })
 export class MyformComponent implements OnInit {
 
-  name = new FormControl();
-  age = new FormControl();
-  address = new FormControl();
-  gender = new FormControl();
+  myform = new FormGroup({
+    name: new FormControl(null, Validators.required),
+    age: new FormControl(32, [Validators.required, Validators.min(18)]),
+    address: new FormControl('Cebu City', Validators.required),
+    gender: new FormControl('Male', Validators.required)
+  })
 
+  disableBtn = false;
 
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  get formName(){
+    return this.myform.get('name')
+  }
+
+  get formAge(){
+    return this.myform.get('age')
+  }
+
+
+  submitForm(form: FormGroup){
+    
+    this.disableBtn = true;
+  
+    let formValue = form.value
+    //  http request
+    this.myform.reset();
+    this.disableBtn = false;
   }
 
 }
